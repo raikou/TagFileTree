121b0e8b7c9e7c87424386da9b32a89c41ecf1e2
rai <rai.create.program.git@gmail.com>
2013-05-12 05:19:54 +0900

    一覧表示の色変更サンプル完了

--- TagFileTree/Form1.Designer.cs
+++ TagFileTree/Form1.Designer.cs
@@ -38,8 +38,15 @@
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.listView1 = new System.Windows.Forms.ListView();
 			this.toolStrip1.SuspendLayout();
 			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// toolStrip1
@@ -113,21 +120,74 @@
 			this.設定ToolStripMenuItem.Size = new System.Drawing.Size(44, 22);
 			this.設定ToolStripMenuItem.Text = "設定";
 			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 52);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+			this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.listView1);
+			this.splitContainer1.Size = new System.Drawing.Size(921, 450);
+			this.splitContainer1.SplitterDistance = 183;
+			this.splitContainer1.TabIndex = 6;
+			// 
+			// treeView1
+			// 
+			this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.treeView1.Location = new System.Drawing.Point(0, 0);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(180, 450);
+			this.treeView1.TabIndex = 0;
+			// 
+			// listView1
+			// 
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView1.FullRowSelect = true;
+			this.listView1.GridLines = true;
+			this.listView1.Location = new System.Drawing.Point(-6, 0);
+			this.listView1.Name = "listView1";
+			this.listView1.OwnerDraw = true;
+			this.listView1.Size = new System.Drawing.Size(740, 450);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.DrawColumnHeader += new System.Windows.Forms.DrawListViewColumnHeaderEventHandler(this.listView1_DrawColumnHeader);
+			this.listView1.DrawItem += new System.Windows.Forms.DrawListViewItemEventHandler(this.listView1_DrawItem);
+			this.listView1.DrawSubItem += new System.Windows.Forms.DrawListViewSubItemEventHandler(this.listView1_DrawSubItem);
+			this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.ClientSize = new System.Drawing.Size(921, 502);
+			this.Controls.Add(this.splitContainer1);
 			this.Controls.Add(this.toolStrip1);
 			this.Controls.Add(this.menuStrip1);
 			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
 			this.Text = "付箋付き木";
+			this.Load += new System.EventHandler(this.Form1_Load);
 			this.toolStrip1.ResumeLayout(false);
 			this.toolStrip1.PerformLayout();
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -144,6 +204,9 @@
 		private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
 		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
 		private System.Windows.Forms.ToolStripButton toolStripButton2;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.ListView listView1;
 	}
 }
 
--- TagFileTree/Form1.cs
+++ TagFileTree/Form1.cs
@@ -8,17 +8,70 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Drawing.Drawing2D;
+using System.Globalization;
+
 namespace TagFileTree
 {
 	public partial class Form1 : Form
 	{
+		const int titleWidth = 300;
+		
 		public Form1()
 		{
 			InitializeComponent();
 			CustomToolsColor CTool = new CustomToolsColor();
 			ToolStripManager.Renderer =	new ToolStripProfessionalRenderer( CTool );
 			this.BackColor = CTool.getBackGroundColor();
+
 		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//リストにデータを追加する。
+			{
+				List<string> headerNames = new List<string>();
+				List<int> headerWidth = new List<int>();
+
+				headerNames.Add("タイトル"); headerWidth.Add( titleWidth );
+				headerNames.Add("追加日時"); headerWidth.Add(100);
+
+				ColumnHeader[] colHeaderNames = new ColumnHeader[headerNames.Count];
+
+				for (int i = 0; i < headerNames.Count; i++)
+				{
+					ColumnHeader columnName = new ColumnHeader();
+					columnName.Text = headerNames[i];
+					columnName.Width = headerWidth[i];
+					colHeaderNames[i] = columnName;
+				}
+
+				listView1.Columns.AddRange(colHeaderNames);
+			}
+
+			//データリスト
+			List<string[]> titles = new List<string[]>();
+
+			//テストデータ追加
+			{
+				for (int i = 0; i < 4; i++)
+				{
+					string[] t1 = { "タイトル-データ番号：", "2012/11/12" };
+					t1[0] += i.ToString();
+					titles.Add(t1);
+				}
+			}
+
+			//データをリストへ登録
+			{
+				foreach (string[] title in titles)
+				{
+					listView1.Items.Add(new ListViewItem(title));
+				}
+			}
+
+		}
+
 
 		private void toolStripDropDownButton1_Click(object sender, EventArgs e)
 		{
@@ -27,6 +80,171 @@ namespace TagFileTree
 
 		private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
 		{
+
+		}
+
+		private void splitter1_SplitterMoved(object sender, SplitterEventArgs e)
+		{
+
+		}
+
+		private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		/// <summary>
+		/// アイテムの背景色を変更する
+		/// タグ表示を追加する・・・が表示が一部消えてしまって使えないので、ゴミ
+		/// 参考：http://www.kisoplus.com/sample/owner/itibu.html
+		/// 参考：http://amonution.sblo.jp/article/48035059.html
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void listView1_DrawItem(object sender, DrawListViewItemEventArgs e)
+		{
+			//適切な色で背景を描画する。
+			//e.DrawBackground();
+			e.DrawFocusRectangle();
+			//string txt = ((ListBox)sender).Items[e.ItemIndex].ToString();
+			string txt = e.Item.Text;
+			string txt2 = txt.Replace("データ", "\n データ \n");
+			Rectangle rec = e.Bounds;
+			rec.X += 10;
+			rec.Height += 0;
+			rec.Width -= 10;
+			Rectangle fullLine = e.Bounds; //行のRECT
+			Graphics g = e.Graphics;
+			Color defForeColor = Color.Black; //文字色のデフォルト
+			Color foreColor = defForeColor;//文字色
+
+
+			//選択時の動作
+			if ((e.State & ListViewItemStates.Selected) != 0)
+			{
+				// Draw the background and focus rectangle for a selected item.
+				//アイテムが洗濯された際の色と範囲を設定している
+				Rectangle rect = e.Bounds;
+				e.Graphics.FillRectangle(Brushes.Blue, rect);
+				e.DrawFocusRectangle();
+				defForeColor = Color.White;
+			}else{
+				//偶数行の色を変える。
+				if (e.ItemIndex % 2 == 1){
+					e.Graphics.FillRectangle(Brushes.SkyBlue, fullLine);
+				}
+			}
+			
+			foreach (string str in txt2.Split('\n'))
+			{
+				//文字幅を計算する
+				rec.Width = TextRenderer.MeasureText(g, str, listView1.Font, new Size(int.MaxValue, int.MinValue), TextFormatFlags.NoPadding).Width;
+
+				//タグ表示部
+				if (str.Equals(" データ "))
+				{
+					foreColor = Color.Red;
+					Color backColor = Color.Blue;//背景色
+					//背景描画
+					e.Graphics.FillRectangle(Brushes.Black, rec);
+					rec.X += 1;
+					rec.Y += 1;
+					rec.Width -= 2;
+					rec.Height -= 2;
+					//背景描画
+					e.Graphics.FillRectangle(Brushes.Blue, rec);
+					//ここで文字列の描画を行う。
+					TextRenderer.DrawText(g, str, listView1.Font, rec, foreColor);
+					rec.Location = new Point(rec.X + 2, rec.Y);
+				}
+				else //タグ以外表示部
+				{
+					foreColor = defForeColor;
+					//ここで文字列の描画を行う。
+					TextRenderer.DrawText(g, str, listView1.Font, rec, foreColor);
+				}
+
+				//次の表示位置へ移動
+				rec.Location = new Point(rec.X + rec.Width + 1, rec.Y);
+			}
+
+			//表示場所を次の位置にする。
+			rec.Location = new Point(rec.X + rec.Width+1, rec.Y);
+			ListViewItem lvs = listView1.Items[ e.ItemIndex ];
+			for( int i=1; i<lvs.SubItems.Count; i++){
+				rec.X = titleWidth + lvs.Position.X;
+				rec.Y = lvs.Position.Y;
+				string text = lvs.SubItems[i].Text;
+				rec.Width = TextRenderer.MeasureText(g, text, listView1.Font, new Size(int.MaxValue, int.MinValue), TextFormatFlags.NoPadding).Width;
+				TextRenderer.DrawText(g, text, listView1.Font, rec, foreColor);
+			}
+			rec.Location = new Point(rec.X + rec.Width + 1, rec.Y);
+			
+			//フォーカスを示す四角形を描画
+			e.DrawFocusRectangle();
+
+		}
+
+		private void listView1_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+		{
+			e.DrawDefault = true;
+		}
+
+		private void listView1_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+		{
+			TextFormatFlags flags = TextFormatFlags.Left;
+
+			//TODO：文字列表示の設定
+
+			using (StringFormat sf = new StringFormat()){
+				// Store the column text alignment, letting it default
+				// to Left if it has not been set to Center or Right.
+				switch (e.Header.TextAlign){
+					case HorizontalAlignment.Center:
+						sf.Alignment = StringAlignment.Center;
+						flags = TextFormatFlags.HorizontalCenter;
+						break;
+					case HorizontalAlignment.Right:
+						sf.Alignment = StringAlignment.Far;
+						flags = TextFormatFlags.Right;
+						break;
+				}
+
+				
+				// Draw the text and background for a subitem with a 
+				// negative value. 
+				double subItemValue;
+				if (e.ColumnIndex > 0 && Double.TryParse( e.SubItem.Text, NumberStyles.Currency, NumberFormatInfo.CurrentInfo, out subItemValue) && subItemValue < 0)
+				{
+					// Unless the item is selected, draw the standard 
+					// background to make it stand out from the gradient.
+					if ((e.ItemState & ListViewItemStates.Selected) == 0){
+						e.DrawBackground();
+					}
+
+					// Draw the subitem text in red to highlight it. 
+					e.Graphics.DrawString(e.SubItem.Text, listView1.Font, Brushes.Blue, e.Bounds, sf);
+
+					//描画位置を取得する。
+					Rectangle rec = e.Bounds;
+					Brush brush = Brushes.DarkBlue;
+					rec.Width += rec.Width + 10;
+					e.Graphics.DrawString(e.SubItem.Text, listView1.Font, brush, rec, sf);
+
+					return;
+				}
+
+
+
+				// Draw normal text for a subitem with a nonnegative 
+				// or nonnumerical value.
+				//e.DrawText(flags);
+			}
 
 		}
 	}
